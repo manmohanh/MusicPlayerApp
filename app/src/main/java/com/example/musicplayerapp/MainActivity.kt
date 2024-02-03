948337b260afd74ce3bbd57d92e2bd2a5be59b9d
Manmohan Hansda <hmanmohan12@gmail.com>
2024-02-03 21:22:02 +0530

    update

--- app/src/main/java/com/example/musicplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/musicplayerapp/MainActivity.kt
@@ -1,11 +1,146 @@
 package com.example.musicplayerapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentUris
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.musicplayerapp.databinding.ActivityMainBinding
+import com.example.musicplayerapp.model.Music
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    companion object {
+        lateinit var musicList: ArrayList<Music>
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+
+                } else {
+
+                }
+            }
+
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) -> {
+
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }
+
+
+
+        musicList = getAllAudio()
+        Log.d("TAG", "onCreate: $musicList")
+        binding.apply {
+            rvMusic.layoutManager = LinearLayoutManager(this@MainActivity)
+            rvMusic.adapter = MusicAdapter(this@MainActivity, musicList)
+        }
+
+        binding.favoriteBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, PlayerActivity::class.java))
+        }
+    }
+
+    @SuppressLint("Recycle", "Range")
+    private fun getAllAudio(): ArrayList<Music> {
+        val audioList = ArrayList<Music>()
+
+        val projection = arrayOf(
+            MediaStore.Audio.AudioColumns._ID,
+            MediaStore.Audio.AudioColumns.TITLE,
+            MediaStore.Audio.AudioColumns.DISPLAY_NAME,
+            MediaStore.Audio.AudioColumns.ARTIST,
+            MediaStore.Audio.AudioColumns.DATA,
+            MediaStore.Audio.AudioColumns.DURATION,
+            MediaStore.Audio.AudioColumns.ALBUM_ID
+        )
+
+
+        val selectionClause: String = "${MediaStore.Audio.AudioColumns.IS_MUSIC} = ?"
+        val selectionArg = arrayOf("1")
+        val sortOrder = "${MediaStore.Audio.AudioColumns.DISPLAY_NAME} ASC"
+
+        val mCursor = this.contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selectionClause,
+            selectionArg,
+            sortOrder
+        )
+
+        mCursor?.use { cursor ->
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.AudioColumns._ID)
+            val titleColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.TITLE)
+            val displayNameColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.DISPLAY_NAME)
+            val artistColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.ARTIST)
+            val dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.DATA)
+            val durationColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.DURATION)
+            val albumIdColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.ALBUM_ID)
+
+            cursor.apply {
+                if (count == 0) {
+
+                } else {
+                    while (cursor.moveToNext()) {
+                        val id = getLong(idColumn)
+                        val title = getString(titleColumn)
+                        val displayName = getString(displayNameColumn)
+                        val artist = getString(artistColumn)
+                        val data = getString(dataColumn)
+                        val duration = getInt(durationColumn)
+                        val albumId = getLong(albumIdColumn)
+
+                        val uri = Uri.parse("content://media/external/audio/albumart")
+                        val artUri = ContentUris.withAppendedId(uri,albumId)
+
+                        audioList += Music(
+                            id = id,
+                            title = title,
+                            displayName = displayName,
+                            artist = artist,
+                            data = data,
+                            duration = duration,
+                            uri = artUri
+                        )
+                    }
+                }
+            }
+        }
+        return audioList
     }
 }
